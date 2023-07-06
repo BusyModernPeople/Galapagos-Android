8dedaf5d27dc45d1882db5f1b4f95c6d76603417
DongChyeon <kimdonghyun90766@gmail.com>
2023-07-06 22:18:57 +0900

    💄Design: 폰트 테마 적용

--- app/src/main/java/com/busymodernpeople/galapagos/MainActivity.kt
+++ app/src/main/java/com/busymodernpeople/galapagos/MainActivity.kt
@@ -3,41 +3,60 @@ package com.busymodernpeople.galapagos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.busymodernpeople.galapagos.ui.theme.GalapagosTheme
+import com.busymodernpeople.galapagos.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GalapagosTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    contentAlignment = Alignment.Center
                 ) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        Text(style = Typography.title1, text = "Android")
+                        Text(style = Typography.title2, text = "Android")
+                        Text(style = Typography.title3, text = "Android")
+                        Text(style = Typography.title4, text = "Android")
+                        Text(style = Typography.body1, text = "Android")
+                        Text(style = Typography.body2, text = "Android")
+                        Text(style = Typography.body3, text = "Android")
+                        Text(style = Typography.body4, text = "Android")
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GalapagosTheme {
-        Greeting("Android")
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Text(style = Typography.title1, text = "Android")
+                Text(style = Typography.title2, text = "Android")
+                Text(style = Typography.title3, text = "Android")
+                Text(style = Typography.title4, text = "Android")
+                Text(style = Typography.body1, text = "Android")
+                Text(style = Typography.body2, text = "Android")
+                Text(style = Typography.body3, text = "Android")
+                Text(style = Typography.body4, text = "Android")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/busymodernpeople/galapagos/ui/theme/Font.kt
@@ -0,0 +1,14 @@
+package com.busymodernpeople.galapagos.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.busymodernpeople.galapagos.R
+
+val Pretendard = FontFamily(
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal)
+)
