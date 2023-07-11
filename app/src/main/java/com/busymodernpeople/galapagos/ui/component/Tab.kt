a96a6ea177bb04a5574ce3ef592a6a6e851fbaa4
DongChyeon <kimdonghyun90766@gmail.com>
2023-07-12 01:12:43 +0900

    💄UI: Glassmorphic Tab 텍스트 크기에 맞게 indicator 크기 자동 조절

--- app/src/main/java/com/busymodernpeople/galapagos/ui/component/Tab.kt
+++ app/src/main/java/com/busymodernpeople/galapagos/ui/component/Tab.kt
@@ -6,21 +6,24 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -44,11 +47,12 @@ private fun TabIndicator(
     )
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun TabItem(
+    modifier: Modifier = Modifier,
     isSelected: Boolean,
     onClick: () -> Unit,
-    tabWidth: Dp,
     text: String
 ) {
     val textColor: Color by animateColorAsState(
@@ -60,18 +64,21 @@ private fun TabItem(
         animationSpec = tween(easing = LinearEasing)
     )
 
-    Text(
-        modifier = Modifier
-            .clip(CircleShape)
-            .clickable { onClick() }
-            .width(tabWidth)
-            .padding(horizontal = 15.dp, vertical = 7.dp),
-        style = Typography.body2,
-        fontWeight = FontWeight.Bold,
-        text = text,
-        color = textColor,
-        textAlign = TextAlign.Center
-    )
+    Surface(
+        modifier = modifier,
+        shape = CircleShape,
+        color = Color.Transparent,
+        onClick = { onClick() },
+        interactionSource = NoRippleInteractionSource()
+    ) {
+        Text(
+            modifier = Modifier.padding(horizontal = 15.dp, vertical = 7.dp),
+            style = Typography.body2,
+            fontWeight = FontWeight.Bold,
+            text = text,
+            color = textColor
+        )
+    }
 }
 
 @Composable
@@ -80,11 +87,16 @@ fun GlassmorphicTab(
     items: List<String>,
     selectedItemIndex: Int,
     indicatorColor: Color = PrimaryGreen,
-    tabWidth: Dp = 67.dp,
     onClick: (index: Int) -> Unit
 ) {
+    val density = LocalDensity.current
+    val sizeList = remember { mutableStateMapOf<Int, Dp>() }
+
     val indicatorOffset: Dp by animateDpAsState(
-        targetValue = tabWidth * selectedItemIndex,
+        targetValue = sizeList
+            .values
+            .take(selectedItemIndex)
+            .sumOf { it },
         animationSpec = tween(easing = LinearEasing)
     )
 
@@ -117,7 +129,7 @@ fun GlassmorphicTab(
 
         Box(modifier = modifier.padding(all = 8.dp)) {
             TabIndicator(
-                indicatorWidth = tabWidth,
+                indicatorWidth = sizeList[selectedItemIndex] ?: 0.dp,
                 indicatorOffset = indicatorOffset,
                 indicatorColor = indicatorColor
             )
@@ -131,15 +143,25 @@ fun GlassmorphicTab(
                     val isSelected = index == selectedItemIndex
 
                     TabItem(
+                        modifier = Modifier.onSizeChanged {
+                            sizeList[index] = with(density) { it.width.toDp() }
+                        },
                         isSelected = isSelected,
                         onClick = { onClick(index) },
-                        tabWidth = tabWidth,
                         text = text
                     )
                 }
             }
         }
     }
+}
+
+inline fun <T> Iterable<T>.sumOf(selector: (T) -> Dp): Dp {
+    var sum: Dp = 0.dp
+    for (element in this) {
+        sum += selector(element)
+    }
+    return sum
 }
 
 @Preview
