00d11080e4fb58a391182655963729612d175688
DongChyeon <kimdonghyun90766@gmail.com>
2023-08-06 16:13:46 +0900

    💄 UI : JoinCompleteScreen 작성

--- app/src/main/java/com/busymodernpeople/galapagos/GalapagosNavHost.kt
+++ app/src/main/java/com/busymodernpeople/galapagos/GalapagosNavHost.kt
@@ -6,13 +6,19 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.busymodernpeople.core.design.ui.component.EnterAnimation
 import com.busymodernpeople.feature.join.JoinAgreeScreen
+import com.busymodernpeople.feature.join.JoinCompleteScreen
 import com.busymodernpeople.feature.join.JoinEmailScreen
+import com.busymodernpeople.feature.join.JoinNicknameScreen
+import com.busymodernpeople.feature.join.JoinPasswordScreen
 import com.busymodernpeople.feature.login.LoginScreen
 
 object GalapagosDestinations {
     const val LOGIN = "login"
     const val JOIN_AGREE = "joinAgree"
     const val JOIN_EMAIL = "joinEmail"
+    const val JOIN_PASSWORD = "joinPassword"
+    const val JOIN_NICKNAME = "joinNickname"
+    const val JOIN_COMPLETE = "joinComplete"
 }
 
 @Composable
@@ -42,7 +48,32 @@ fun GalapagosNavHost() {
             EnterAnimation {
                 JoinEmailScreen(
                     onBack = { navController.navigateUp() },
-                    onConfirm = { }
+                    onConfirm = { navController.navigate(GalapagosDestinations.JOIN_PASSWORD) }
+                )
+            }
+        }
+        composable(route = GalapagosDestinations.JOIN_PASSWORD) {
+            EnterAnimation {
+                JoinPasswordScreen(
+                    onBack = { navController.navigateUp() },
+                    onConfirm = { navController.navigate(GalapagosDestinations.JOIN_NICKNAME) }
+                )
+            }
+        }
+        composable(route = GalapagosDestinations.JOIN_NICKNAME) {
+            EnterAnimation {
+                JoinNicknameScreen(
+                    onBack = { navController.navigateUp() },
+                    onConfirm = { navController.navigate(GalapagosDestinations.JOIN_COMPLETE)}
+                )
+            }
+        }
+        composable(route = GalapagosDestinations.JOIN_COMPLETE) {
+            EnterAnimation {
+                JoinCompleteScreen(
+                    onBack = { navController.navigateUp() },
+                    onRegisterPet = { },
+                    onGuestMode = { }
                 )
             }
         }
